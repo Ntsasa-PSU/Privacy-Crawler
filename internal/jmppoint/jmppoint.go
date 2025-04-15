134fbdb8590d3456afa76d735385d4ad07ad7fe7
TheIcarus <qntsasa@gmail.com>
2025-04-14 19:30:03 -0700

    +Skeleton for PROMETHEUS server

---
+++ internal/jmppoint/jmppoint.go
@@ -0,0 +1,12 @@
+package jmppoint
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+
