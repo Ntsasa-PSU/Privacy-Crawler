56e7c610b3ac2cdea6a1a30e95c74650fcc32235
TheIcarus <qntsasa@gmail.com>
2025-04-14 19:43:55 -0700

    Save

--- internal/jmppoint/jmppoint.go
+++ internal/jmppoint/jmppoint.go
@@ -1,12 +1,50 @@
 package jmppoint
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+    //"fmt"
+    //"net/http"
+    //"time"
 
+	//"privcrawler/internal/crawler"
     "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+    //"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// ---- DATA STRUCTURES ---- //
+
+//Metrics: testing structure for prometheus metrics
+var (
+	// httpRequests: Counter for total number of HTTP requests.
+    httpRequests = prometheus.NewCounterVec(
+        prometheus.CounterOpts{
+            Name: "http_requests_total",
+            Help: "Total number of HTTP requests",
+        },
+        []string{"path"},
+    )
+
+	// requestDuration: Histogram for response time of HTTP requests.
+    requestDuration = prometheus.NewHistogramVec(
+        prometheus.HistogramOpts{
+            Name:    "http_request_duration_seconds",
+            Help:    "Histogram of response time for handler",
+            Buckets: prometheus.DefBuckets,
+        },
+        []string{"path"},
+    )
 )
 
 
+
+// ---- Functions ---- //
+ 
+// Function: Initialize 
+// Operation: This will initialize the Prometheus metrics and register them with the default registry.
+// Return: None
+func init() {
+    // Register metrics
+    prometheus.MustRegister(httpRequests)
+    prometheus.MustRegister(requestDuration)
+}
+
+// Function 
