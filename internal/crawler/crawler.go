6e806905584437c34239ccec8d44c5f9c68243ae
Q Ntsasa <ntsasa@cecs.pdx.edu>
2025-04-30 19:46:33 -0700

    +Merged Crawler code +Added configuration skeleton

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -7,6 +7,11 @@ import (
 	"io"
 	"time"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/playwright-community/playwright-go"
 )
 
 
@@ -20,6 +25,12 @@ type URLList struct {
 	URLs []string `json:"urls"`
 }
 
+// CookieList: Represents the List of cookies collected.
+// Uses type Cookie for each entry.
+type CookiesList struct {
+	List map[string][]Cookie
+}
+
 // Result: Represent the outcome from fetching URL.
 type Result struct {
 	URL      string
@@ -29,6 +40,18 @@ type Result struct {
 	Error    error
 }
 
+// Cookie: Represents the fields of a Cookie.
+type Cookie struct {
+	Name         string
+	Value        string
+	Domain       string
+	Path         string
+	Expires      float64
+	HttpOnly     bool
+	Secure       bool
+	SameSite     string
+	IsFirstParty bool
+}
 
 
 // ---- Global Definitions ---- //
@@ -109,8 +132,8 @@ func GetBrowsers(verbose *bool) map[string]string {
 
 // Fucntion: Verify Browser
 // Operation: Verify we have an agent for selected browser.
-// Return: String(Browser found)
-func VerifyTargetBrowser(browsers map[string]string, selectedBrowser string, verbose *bool) string {
+// Return: String(Browser found), String (User-Agent), Error
+func VerifyTargetBrowser(browsers map[string]string, selectedBrowser string, verbose *bool) (string, string, error) {
 
 	// - Verbose Output - //
 	if *verbose {
@@ -119,80 +142,274 @@ func VerifyTargetBrowser(browsers map[string]string, selectedBrowser string, ver
 	}
 
 	// Iterate through map of browsers.
-	for browserName := range browsers {
+	for browserName, browserUserAgent := range browsers {
 
 		if browserName == selectedBrowser {
 
 			// - Verbose Output - //
 			if *verbose {
+
 				fmt.Printf("Found: %s\n", browserName)
 			}
 
-			return browserName
+			return browserName, browserUserAgent, nil
 		}
 	}
 
-	fmt.Printf("Error: Failure to find browser.\n")
-	return "None"
+	return "None", "None", fmt.Errorf("browser not found")
 }
+
 
 // Fucntion: Fetch URL
 // Operation: Connect and returns header from selected URL.
-// Return: Header (map), Status Code (int), Error
+// Return: Header (map), Body (map), Status Code (int), Error
+func FetchPacket(url string, userAgent string, verbose *bool) (map[string][]string, []byte, int, error) {
 
-// Note: browser is just a string in field for "user-agent"
-func FetchHeaders(url string, browser string, verbose *bool) (map[string][]string, int, error) {
-    
 	// Create a new HTTP client.
 	client := &http.Client{
 		// Timeout if cannot connect.
-        Timeout: time.Second * 10,
-    }
-    
-    // Create a new request.
-	request, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-        return nil, 0, fmt.Errorf("error creating request: %v", err)
-    }
-    
-    // Set the User-Agent header based on browser.
-	request.Header.Set("User-Agent", browser)
-
-    // Make the request.
-	response, err := client.Do(request)
-    if err != nil {
-        return nil, 0, fmt.Errorf("error making request: %v", err)
-    }
-	
-	// - Verbose output - //
-	if *verbose {
-		// Read the response body
-		bodyBytes, err := io.ReadAll(response.Body)
-			if err != nil {
-    		return response.Header, response.StatusCode, fmt.Errorf("error reading response body: %v", err)
-		}
-		// Print the response body
-		fmt.Printf("\n-- Response body -- \n%s\n", bodyBytes)
+		Timeout: time.Second * 10,
 	}
-	// Close the response.
+
+	// Create a new request.
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("error creating request: %v", err)
+	}
+
+	// Set the User-Agent header based on browser.
+	request.Header.Set("User-Agent", userAgent)
+
+	// Make the request.
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("error making request: %v", err)
+	}
+
+	// Cleanup on response.
 	defer response.Body.Close()
 
-   
-	
-   // - Verbose output - //
+	// Read the body.
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, nil, 0, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// Cleanup on body.
+	response.Body.Close()
+
+	// - Verbose output - //
 	if *verbose {
-		fmt.Printf("\n-- HTTP Headers for %s --\n", url)
+		fmt.Printf("\n-- HTTP Packet: Header for %s --\n", url)
 		fmt.Printf("Status: %s\n", response.Status)
+
+		fmt.Printf("\n-- HTTP Packet: Body for %s --\n\n", url)
+		fmt.Printf("Body: %s\n", string(body))
 	}
 
 	// Return the headers and status code.
-	return response.Header, response.StatusCode, nil
+	return response.Header, body, response.StatusCode, nil
+}
+
+// Function: Fetch Cookies
+// Operation: Connect to url with browser, creates cookies using playwright
+// to fully generate all cookies due to Javascript delys.
+//
+// Return: A list of cookies collected and stored in a struct (*CookiesList)
+func FetchCookies(browser string, isHidden bool, url string, verbose *bool) *CookiesList {
+	// - Run Playwright - //
+	pw, err := playwright.Run()
+	if err != nil {
+		fmt.Printf("could not lauch playwright: %v", err)
+		return nil
+	}
+	defer pw.Stop()
+
+	// Declare launcher ahead of time
+	var launcher playwright.Browser
+
+	// launching specific browser (edge is missing)
+	if browser == "chrome" {
+		launcher, err = pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+			Headless: playwright.Bool(isHidden),
+		})
+	} else if browser == "firefox" {
+		launcher, err = pw.Firefox.Launch(playwright.BrowserTypeLaunchOptions{
+			Headless: playwright.Bool(isHidden),
+		})
+	} else if browser == "safari" {
+		launcher, err = pw.WebKit.Launch(playwright.BrowserTypeLaunchOptions{
+			Headless: playwright.Bool(isHidden),
+		})
+	} else if browser == "edge" {
+        launcher, err = pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+            Channel: playwright.String("msedge"),
+            Headless: playwright.Bool(isHidden),
+        })
+    } else {
+		fmt.Printf("Browser %s is not compativle\n", browser)
+		return nil
+	}
+	if err != nil {
+		fmt.Printf("could not launch browser: %s\n", browser)
+		return nil
+	}
+
+	if *verbose {
+		fmt.Printf("Launching %s...\n", browser)
+		if isHidden {
+			fmt.Println("\t- Browser is hidden.")
+		}
+		fmt.Println("\t- Browser is visible.")
+	}
+
+	// Create the context for the broswer
+	context, err := launcher.NewContext() // creates an isolated browsers contents
+	if err != nil {
+		fmt.Printf("could not create context: %v", err)
+		return nil
+	}
+
+	// Open up a new tab from the context
+	page, err := context.NewPage() // Add a new Tab
+	if err != nil {
+		fmt.Printf("could not create a new Tab: %v", err)
+		return nil
+	}
+
+	// Navigate to the desired URL
+	_, err = page.Goto(url)
+	if err != nil {
+		fmt.Printf("could not go to url page: %v", err)
+	}
+
+	// Wait for a few seconds for JS to run and set cookies
+	page.WaitForTimeout(5000)
+
+	// Get cookies
+	cookies, err := context.Cookies()
+	if err != nil {
+		fmt.Printf("could not get cookies: %v", err)
+		return nil
+	}
+	if len(cookies) == 0 {
+		fmt.Println("No cookies were returned.")
+		return nil
+	}
+
+	// Store cookies in a struc, organized
+	collectedCookies := CookiesList{
+		List: make(map[string][]Cookie),
+	}
+
+	for i, c := range cookies {
+		// Check for first or third part
+		isFirst := isFirstParty(c.Domain, url) // checks if url and cleanDomain are the same
+
+		// Store fields inside of a cookie
+		cookie := Cookie{
+			Name:         c.Name,
+			Value:        c.Value,
+			Domain:       c.Domain,
+			Path:         c.Path,
+			Expires:      float64(c.Expires),
+			HttpOnly:     c.HttpOnly,
+			Secure:       c.Secure,
+			SameSite:     string(*c.SameSite),
+			IsFirstParty: isFirst,
+		}
+		// Map to Cookie Key
+		collectedCookies.List[c.Domain] = append(collectedCookies.List[c.Domain], cookie)
+
+		if *verbose {
+			fmt.Printf("*** Cookies Collected: %d ***\n", i+1)
+		}
+	}
+
+	return &collectedCookies
+}
+
+// Function: Print Cookies
+// Operation: Print the cookies in a readable format
+// Return: None
+func PrintCookies(cookieList *CookiesList, fromPage string, verbose *bool) {
+	if cookieList == nil || len(cookieList.List) == 0 {
+		fmt.Println("No cookies to display.")
+		return
+	}
+
+	fmt.Println("\n-- Cookies Displayed --")
+
+	cookieCount := 0
+	for domain, cookies := range cookieList.List {
+		fmt.Printf("Domain: %s\n", domain)
+
+		for i, cookie := range cookies {
+			partyType := "third-party"
+			fmt.Printf("\tCookie %d.[%s] \n", i+1, partyType)
+			if cookie.IsFirstParty {
+				partyType = "first-party"
+			}
+
+			fmt.Printf("\t\tFrom Page: %s\n", fromPage)
+			fmt.Printf("\t\tName: %s\n", cookie.Name)
+			fmt.Printf("\t\tValue: %s\n", cookie.Value)
+			fmt.Printf("\t\tPath: %s\n", cookie.Path)
+
+			// Check if Expires is greater than 0 to avoid negative time
+			// Expires is a float64, so we convert it to int64 for Unix time
+			if cookie.Expires > 0 {
+				expTime := time.Unix(int64(cookie.Expires), 0)
+				fmt.Printf("\t\tExpires: %s\n", expTime.Format("Mon, 02 Jan 2006"))
+			} else {
+				fmt.Println("\t\tExpires: No Expiration")
+			}
+
+			fmt.Printf("\t\tHttpOnly: %t\n", cookie.HttpOnly)
+			fmt.Printf("\t\tSecure: %t\n", cookie.Secure)
+			fmt.Printf("\t\tSameSite: %s\n", cookie.SameSite)
+			cookieCount++
+		}
+
+		fmt.Println("#-------------------------------------#")
+	}
+
+	if *verbose {
+		fmt.Printf("Total Cookies Collected: %d\n", cookieCount)
+	}
+}
+
+// Function: isFirstParty
+// Operation: Check if the cookie domain is first-party or third-party
+// Return: True if first-party, false if third-party
+func isFirstParty(cookieDomain, fulURL string) bool {
+	parsedURL, err := url.Parse(fulURL)
+	if err != nil {
+		fmt.Printf("could not parse URL: %v", err)
+		return false
+	}
+
+	topLevelDomain := parsedURL.Host                           // Get the domain from the URL (e.g., "www.amazon.com")
+	cleanCookieDomain := strings.TrimPrefix(cookieDomain, ".") // TrimPrefix: removes "." from the start of cookieDomain
+
+	// HasPrefix: checks if the cleanCookieDomain STARTS with the topLevelDomain.
+	isPrefix := strings.HasPrefix(cleanCookieDomain, topLevelDomain)
+	// HasSuffix: checks if the topLevelDomain ENDS with the cleanCookieDomain.
+	isSuffix := strings.HasSuffix(topLevelDomain, cleanCookieDomain)
+
+	return isSuffix || isPrefix
 }
 
 
 
-// Next Step 1: Handle HTTP Headers
-//    3. Parse HTTP Headers
+// Make PrivacyMetric Struct
+// Make function to import packet data into struct.
+
+//Then can make application do all borwsers+ urls
+// Host on server
+// Can figure out where to host
+
+//Once data gathered, analyze it? and make a report
 
 // Next Step 2: Create Server Go agents can connect to
 //    1. Create a server that can analyze the connection
